f3ad44c863fda3cc7598ba67acf4a5f1a4017410
ken <kenulborapublic@gmail.com>
2017-08-16 23:07:42 -0400

    updates

--- services/contentService.go
+++ services/contentService.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"bytes"
+	b64 "encoding/base64"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -36,12 +37,15 @@ type Content struct {
 type Response struct {
 	Success bool  `json:"success"`
 	ID      int64 `json:"id"`
+	Code    int   `json:"code"`
 }
 
 //AddContent add content
 func (c *ContentService) AddContent(content *Content) *Response {
 	var rtn = new(Response)
 	var addURL = c.Host + "/rs/content/add"
+	content.Text = b64.StdEncoding.EncodeToString([]byte(content.Text))
+	//fmt.Println(content.Text)
 	aJSON, err := json.Marshal(content)
 	if err != nil {
 		fmt.Println(err)
@@ -66,8 +70,104 @@ func (c *ContentService) AddContent(content *Content) *Response {
 				if error != nil {
 					log.Println(error.Error())
 				}
+				rtn.Code = resp.StatusCode
 			}
 		}
+	}
+	return rtn
+}
+
+//UpdateContent update content
+func (c *ContentService) UpdateContent(content *Content) *Response {
+	var rtn = new(Response)
+	var upURL = c.Host + "/rs/content/update"
+	content.Text = b64.StdEncoding.EncodeToString([]byte(content.Text))
+	//fmt.Println(content.Text)
+	aJSON, err := json.Marshal(content)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		req, rErr := http.NewRequest("PUT", upURL, bytes.NewBuffer(aJSON))
+		if rErr != nil {
+			fmt.Print("request err: ")
+			fmt.Println(rErr)
+		} else {
+			req.Header.Set("Content-Type", "application/json")
+			req.Header.Set("Authorization", "Bearer "+c.Token)
+			req.Header.Set("clientId", c.ClientID)
+			client := &http.Client{}
+			resp, cErr := client.Do(req)
+			if cErr != nil {
+				fmt.Print("Content Service Update err: ")
+				fmt.Println(cErr)
+			} else {
+				defer resp.Body.Close()
+				decoder := json.NewDecoder(resp.Body)
+				error := decoder.Decode(&rtn)
+				if error != nil {
+					log.Println(error.Error())
+				}
+				rtn.Code = resp.StatusCode
+			}
+		}
+	}
+	return rtn
+}
+
+// //UpdateContent update content
+// func (c *ContentService) UpdateContentHits(content *Content) *Response {
+// 	var rtn = new(Response)
+// 	var upURL = c.Host + "/rs/content/update"
+// 	content.Text = b64.StdEncoding.EncodeToString([]byte(content.Text))
+// 	//fmt.Println(content.Text)
+// 	aJSON, err := json.Marshal(content)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	} else {
+// 		req, rErr := http.NewRequest("PUT", upURL, bytes.NewBuffer(aJSON))
+// 		if rErr != nil {
+// 			fmt.Print("request err: ")
+// 			fmt.Println(rErr)
+// 		} else {
+// 			req.Header.Set("Content-Type", "application/json")
+// 			req.Header.Set("Authorization", "Bearer "+c.Token)
+// 			req.Header.Set("clientId", c.ClientID)
+// 			client := &http.Client{}
+// 			resp, cErr := client.Do(req)
+// 			if cErr != nil {
+// 				fmt.Print("Content Service Update err: ")
+// 				fmt.Println(cErr)
+// 			} else {
+// 				defer resp.Body.Close()
+// 				decoder := json.NewDecoder(resp.Body)
+// 				error := decoder.Decode(&rtn)
+// 				if error != nil {
+// 					log.Println(error.Error())
+// 				}
+// 				rtn.Code = resp.StatusCode
+// 			}
+// 		}
+// 	}
+// 	return rtn
+// }
+
+// GetContent get content
+func (c *ContentService) GetContent(id string, clientID string) *Content {
+	var rtn = new(Content)
+	var gURL = c.Host + "/rs/content/get/" + id + "/" + clientID
+	fmt.Println(gURL)
+	resp, err := http.Get(gURL)
+	fmt.Println(resp)
+	if err != nil {
+		panic(err)
+	} else {
+		defer resp.Body.Close()
+		decoder := json.NewDecoder(resp.Body)
+		error := decoder.Decode(&rtn)
+		if error != nil {
+			log.Println(error.Error())
+		}
+		//base64 decode text
 	}
 	return rtn
 }
